205f05a07038eb0e9c5101e04f0ed8e1d7dbec2b
jtmccr1 <mccronejt@gmail.com>
2020-04-09 14:43:38 +0100

    Updates

---
+++ precache-manifest.f350cd697bd3dc9c9ecf8ac94664e926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cf54d4495dbbd5c2f0d6d27fbe45f0",
+    "url": "/SARS-CoV-2_Guangdong_genomic_epidemiology/index.html"
+  },
+  {
+    "revision": "75584675bffc2729a35e",
+    "url": "/SARS-CoV-2_Guangdong_genomic_epidemiology/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3e9a29c35ec84caace12",
+    "url": "/SARS-CoV-2_Guangdong_genomic_epidemiology/static/js/2.6cb929b0.chunk.js"
+  },
+  {
+    "revision": "5c57509335e80f5060d35ed2d166c39e",
+    "url": "/SARS-CoV-2_Guangdong_genomic_epidemiology/static/js/2.6cb929b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75584675bffc2729a35e",
+    "url": "/SARS-CoV-2_Guangdong_genomic_epidemiology/static/js/main.d0101926.chunk.js"
+  },
+  {
+    "revision": "769758047e5e6fafd630",
+    "url": "/SARS-CoV-2_Guangdong_genomic_epidemiology/static/js/runtime-main.30187842.js"
+  }
+]);
